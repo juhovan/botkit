9330e13b9297bc8b357a26a6de2557a22d13fe44
ChanhLeNguyen <chanhnguyen171@gmail.com>
2016-02-22 14:46:08 +0200

    Add who made you 3

--- bot.js
+++ bot.js
@@ -135,7 +135,7 @@ controller.hears(['what is my name','who am i'],'direct_message,direct_mention,m
 controller.hears(['who make you','who made you'],'direct_message,direct_mention,mention',function(bot, message) {
 
     controller.storage.users.get(message.user,function(err, user) {
-        bot.reply(message,'Metro group made me');
+        bot.reply(message,'Metro group made me. Hihi');
     });
 });
 
