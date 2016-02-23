f4bdf24b2fedcd5b8d3c4750a387969609dd6215
Oleg Batalin <oleg.batalin@metropolia.fi>
2016-02-23 11:57:11 +0300

    merge prime branch

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
-        name: 'robot_face',
+        name: 'robot_face'
     },function(err, res) {
         if (err) {
             bot.botkit.log('Failed to add emoji reaction :(',err);
